4b4e17f475bf71970e5c34f85876a5ced8013604
Jerick <evajerick21@gmail.com>
2024-06-26 23:30:54 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var Vehicle = /** @class */ (function () {
+    function Vehicle(model) {
+        this.modelType = model;
+    }
+    return Vehicle;
+}());
+var model_type = {
+    type: 'Car',
+    color: 'Blue',
+    event: function () {
+        console.log('Event Function');
+    }
+};
+var car = new Vehicle(model_type);
+console.log(car);
---
+++ app.ts
@@ -0,0 +1,25 @@
+interface ModelType {
+    type: string,
+    color: string,
+    event: ()=> void;
+}
+
+class Vehicle {    
+    modelType: ModelType
+    constructor(model: ModelType){
+        this.modelType = model;
+    }
+}
+
+
+let model_type = {
+    type : 'Car',
+    color: 'Blue',
+    event: ()=>{
+        console.log('Event Function');
+    }
+}
+
+const  car = new Vehicle(model_type);
+
+console.log(car);
